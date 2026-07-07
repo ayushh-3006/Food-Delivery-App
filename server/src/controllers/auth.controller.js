d60f22a130a9217638e5981925bb689f96b3f8fe
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-07-07 19:59:05 +0530

    feat: Refactor authentication controller by removing commented code and enhancing photo handling in user registration and login

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,94 +1,3 @@
-// import User from "../models/user.model.js";
-// import bcrypt from "bcrypt";
-// import { genToken } from "../utils/Auth.service.js";
-
-// export const RegisterUser = async (req, res, next) => {
-//   try {
-//     const { fullName, email, password, phone, gender, dob } = req.body;
-
-//     if (!fullName || !email || !password || !phone || !gender || !dob) {
-//       const error = new Error("All fields Required");
-//       error.statusCode = 400;
-//       return next(error);
-//     }
-
-//     const existingUser = await User.findOne({ email });
-//     if (existingUser) {
-//       const error = new Error("Email already registred");
-//       error.statusCode = 409;
-//       return next(error);
-//     }
-
-//     const photo = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
-
-//     const SALT = await bcrypt.genSalt(10);
-//     const hashedPassword = await bcrypt.hash(password, SALT);
-
-//     const newUser = await User.create({
-//       fullName,
-//       email,
-//       password: hashedPassword,
-//       phone,
-//       gender,
-//       dob,
-//       photo,
-//     });
-
-//     res.status(201).json({ message: "User Created Successfully" });
-//   } catch (error) {
-//     console.log(error.message);
-//     next();
-//   }
-// };
-
-// export const LoginUser = async (req, res, next) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     if (!email || !password) {
-//       const error = new Error("All fields Required");
-//       error.statusCode = 400;
-//       return next(error);
-//     }
-
-//     const existingUser = await User.findOne({ email });
-//     if (!existingUser) {
-//       const error = new Error("Email not registred");
-//       error.statusCode = 404;
-//       return next(error);
-//     }
-
-//     const isVerified = await bcrypt.compare(password, existingUser.password);
-//     if (!isVerified) {
-//       const error = new Error("Incorrect Password");
-//       error.statusCode = 401;
-//       return next(error);
-//     }
-
-//     await genToken(existingUser);
-
-//     res.status(200).json({
-//       message: "Welcome Back",
-//       data: existingUser,
-//     });
-//   } catch (error) {
-//     console.log(error.message);
-//     next();
-//   }
-// };
-
-// export const LogoutUser = async (req, res, next) => {
-//   try {
-//     //Controller Logic
-//     res.clearCookie("Oreo", { maxAge: 0 });
-
-//     res.status(200).json({ message: "Logout Sucessfully" });
-//   } catch (error) {
-//     console.log(error.message);
-//     next();
-//   }
-// };
-
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import { genToken } from "../utils/Auth.service.js";
@@ -111,7 +20,7 @@ export const RegisterUser = async (req, res, next) => {
     }
 
     const photoURL = `https://placehold.co/600x400?text=${fullName.charAt(0).toUpperCase()}`;
-     const photo = {
+    const photo = {
       url: photoURL,
       publicId: null,
     };
@@ -156,6 +65,10 @@ export const LoginUser = async (req, res, next) => {
       return next(error);
     }
 
+    if (newPhoto) {
+      existingUser?.photo?.publicId &&
+        (await cloudinary.uploaded.destroy(existingUser.photo.publicId));
+    }
     const isVerified = await bcrypt.compare(password, existingUser.password);
     if (!isVerified) {
       const error = new Error("Incorrect Password");
