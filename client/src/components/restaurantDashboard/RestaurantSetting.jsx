42c8aa82f30622502e3c21f9bacbd0a0b3fbf24a
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-07-23 19:38:40 +0530

    feat: Add RestaurantMenu, RestaurantAddress components and enhance RestaurantSetting with data fetching and open status management

--- client/src/components/restaurantDashboard/RestaurantSetting.jsx
+++ client/src/components/restaurantDashboard/RestaurantSetting.jsx
@@ -1,17 +1,95 @@
-import React, { useState } from "react";
-import RestaurantInformation from "./settings/RestaurantInformation";
+import React, { useState, useEffect } from "react";
+import api from "../../config/ApiConfig";
+import toast from "react-hot-toast";
+import { RiLoader4Fill } from "react-icons/ri";
+import { useAuth } from "../../context/AuthContext";
 import ResturantCoreDetails from "./settings/ResturantCoreDetails";
+import Information from "./settings/restaurantinformation/index";
 import RestaurantPhotos from "./settings/RestaurantPhotos";
+import Loader from "../../assets/runningLoader.gif";
+import { IoMdHammer } from "react-icons/io";
 
 const RestaurantSetting = () => {
+   const { user } = useAuth();
   const Tabs = [
     { id: "information", label: "Information" },
     { id: "coreDetails", label: "Core Details" },
     { id: "photos", label: "Photos" },
   ];
-  const [activeTab, setActiveTab] = useState("coreDetails");
+  const [activeTab, setActiveTab] = useState("information");
 
-  const [isRestaurantOpen, setIsRestaurantOpen] = useState(true);
+  const [isLoadingResturantOpen, setIsLoadingResturantOpen] = useState(true);
+  const [isRestaurantOpen, setIsRestaurantOpen] = useState(
+    sessionStorage.getItem("RestaurantOpen") || false,
+  );  
+
+  //Load Restaurant Data
+  const [isLoadingRestaurant, setIsLoadingRestaurant] = useState(false);
+  const [loadingRestaurantError, setLoadingRestaurantError] = useState(null);
+  const [restaurantData, setRestaurantData] = useState();
+
+  const fetchRestaurantData = async () => {
+    try {
+      setIsLoadingRestaurant(true);
+      setIsLoadingResturantOpen(true);
+
+      const res = await api.get(
+        `/restaurant/get-resturant-data?id=${user._id}`,
+      );
+      setRestaurantData(res.data.data);
+      sessionStorage.setItem(
+        "cravingRestaurant",
+        JSON.stringify(res.data.data),
+      );
+      sessionStorage.setItem("RestaurantOpen", res.data.data.isOpen);
+
+      setIsRestaurantOpen(res.data.data.isOpen);
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "Unknown error occurred fetching restaurant. Please try again.",
+      );
+      setLoadingRestaurantError(
+        error.response?.data?.message ||
+          "Unknown error occurred fetching restaurant. Please try again.",
+      );
+    } finally {
+      setIsLoadingRestaurant(false);
+      setIsLoadingResturantOpen(false);
+    }
+  };
+
+  const handleRestaurantOpen = async () => {
+    try {
+      setIsLoadingResturantOpen(true);
+
+      const res = await api.patch(
+        `/restaurant/change-open-status/${!isRestaurantOpen}?id=${user._id}`,
+      );
+      setIsRestaurantOpen(res.data.data.isOpen);
+      setRestaurantData(res.data.data);
+      sessionStorage.setItem(
+        "cravingRestaurant",
+        JSON.stringify(res.data.data),
+      );
+
+      toast.success(res.data.message);
+    } catch (error) {
+      toast.error(
+        error.response?.data?.message ||
+          "Unknown error occurred while Opening the Restaurant. Please try again.",
+      );
+    } finally {
+      setIsLoadingResturantOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchRestaurantData();
+  }, [user]);
+
+  console.log(isRestaurantOpen);
+
   return (
     <>
       <div className=" h-full flex flex-col">
