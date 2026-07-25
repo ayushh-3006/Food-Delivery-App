c0afab8885d159c9f48e6a10cda2db5f5c2ef016
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-07-25 18:00:00 +0530

    Update auth controller and email config

--- server/src/config/email.config.js
+++ server/src/config/email.config.js
@@ -1,11 +1,14 @@
 import nodemailer from "nodemailer";
-
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
 const sendEmail = async (to, subject, message) => {
   try {
-    console.log("Started Sending Email");
+    //   console.log("Started Sending Email");
+
+    //    console.log(process.env.GMAIL_USERNAME);
+
+    //    console.log(process.env.GMAIL_PASSCODE);
 
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -15,28 +18,30 @@ const sendEmail = async (to, subject, message) => {
       },
     });
 
-    console.log("3....2....1....");
+    //   console.log("3....2....1....");
 
     const mailOption = {
-      from: process.env.GMAIL_USER,
+      from: process.env.GMAIL_USERNAME,
       to,
       subject,
       html: message,
     };
 
-    console.log("Sending Email");
+    //   console.log("Sending Email");
 
     const res = await transporter.sendMail(mailOption);
     console.log(res);
   } catch (error) {
     console.log(error);
+    throw error;
   }
 };
 
 export default sendEmail;
 
 // sendEmail(
-//   "rajvardhan@ricr.in",
+//   "receiversemail@gmail.com",
 //   "test Email",
-//   "<p style='color:red;'> Test Message</p>",
+//   `<h1 style='color:blue;'>Test Message</h1>
+//   <p style='color:red;'>Batch 6</p>`,
 // );
