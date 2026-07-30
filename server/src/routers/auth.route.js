a9d7c2a053715626b121fad72c412cb477f7241e
ayushh-3006 <ayushkumarsingh3006@gmail.com>
2026-07-30 18:05:54 +0530

    add customer model and admin routes

--- server/src/routers/auth.route.js
+++ server/src/routers/auth.route.js
@@ -3,7 +3,12 @@ import {
   LoginUser,
   LogoutUser,
   RegisterUser,
+   SendOtp,
+  VerifyOtp,
+  ResetPassword,
 } from "../controllers/auth.controller.js";
+
+import { OTPAuthProtect } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -11,5 +16,7 @@ router.post("/register", RegisterUser);
 router.post("/login", LoginUser);
 router.get("/logout", LogoutUser);
 
-
+router.post("/send-otp", SendOtp);
+router.post("/verify-otp", VerifyOtp);
+router.post("/reset-password", OTPAuthProtect, ResetPassword);
 export default router;
